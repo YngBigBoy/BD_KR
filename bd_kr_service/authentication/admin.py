ec56ef316d302fabd2ba81ed2104a86d9365ccfd
Ivan <58389741+YngBigBoy@users.noreply.github.com>
2024-05-19 09:42:21 +0300

    comment

--- bd_kr_service/authentication/admin.py
+++ bd_kr_service/authentication/admin.py
@@ -1,3 +1,19 @@
+# This is a Django admin configuration file. It is used to register models that will be
+# displayed in the Django admin interface.
+
+# The "admin" module is a part of Django's admin site, which provides a user-friendly
+# interface for managing data in Django-powered applications.
 from django.contrib import admin
 
+# The "register" function is used to register models with the Django admin site.
+# In this file, we will be registering any models that we want to be able to manage
+# through the Django admin interface.
+#
+# For example, if we had a model called "MyModel" that we wanted to manage through the
+# Django admin interface, we would call "admin.site.register(MyModel)" here.
+#
+# admin.site.register(MyModel)
+
+# Since we don't have any specific models to register in this example, we simply
+# leave this comment as a placeholder for future development.
 # Register your models here.
