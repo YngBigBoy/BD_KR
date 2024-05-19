fa231f05b46b99c10ab27ea5b8ff7d4358637c7e
Ivan <58389741+YngBigBoy@users.noreply.github.com>
2024-05-19 09:42:19 +0300

    comment

--- bd_kr_service/analytics/apps.py
+++ bd_kr_service/analytics/apps.py
@@ -1,6 +1,12 @@
 from django.apps import AppConfig
 
-
+# This is an AppConfig class for the 'analytics' app in a Django project.
+# The AppConfig class is used to configure and customize the behavior of a Django app.
 class AnalyticsConfig(AppConfig):
+    # The default_auto_field attribute specifies the default auto-incrementing field type
+    # for models in this app. In this case, it is set to 'django.db.models.BigAutoField',
+    # which is a 64-bit integer field that automatically increments.
     default_auto_field = 'django.db.models.BigAutoField'
+    
+    # The name attribute specifies the name of this app, which is 'analytics'.
     name = 'analytics'
