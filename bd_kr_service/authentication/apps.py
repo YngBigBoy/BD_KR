00b774336f3e0e225a4da1f9ca1d862a61ff8bbc
Ivan <58389741+YngBigBoy@users.noreply.github.com>
2024-05-19 09:42:22 +0300

    comment

--- bd_kr_service/authentication/apps.py
+++ bd_kr_service/authentication/apps.py
@@ -1,6 +1,13 @@
 from django.apps import AppConfig
 
-
+# The AuthenticationConfig class is a subclass of AppConfig for the authentication app.
 class AuthenticationConfig(AppConfig):
+    # The default_auto_field attribute specifies the default auto-incrementing integer field type
+    # for models in this app. In this case, it is set to Django's BigAutoField, which is an integer
+    # field that uses 8 bytes to store the value.
     default_auto_field = 'django.db.models.BigAutoField'
+    
+    # The name attribute is required for all AppConfig subclasses and should be set to the
+    # Python module name for the app. Here, it is set to 'authentication', which is the name
+    # of the Django app that this AppConfig configuration applies to.
     name = 'authentication'
