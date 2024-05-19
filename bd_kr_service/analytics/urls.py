fefb9507e027a33aaf1c1f0d3835058d678ab363
Ivan <58389741+YngBigBoy@users.noreply.github.com>
2024-05-19 09:42:19 +0300

    comment

--- bd_kr_service/analytics/urls.py
+++ bd_kr_service/analytics/urls.py
@@ -1,7 +1,12 @@
+# Import the path function from Django's urls module and the views module from this application
 from django.urls import path
 from . import views
 
+# Define the URL patterns for this application's views
 urlpatterns = [
+    # The empty path maps to the index view function in the views module
     path('', views.index, name="analytics"),
+
+    # The path 'add-expense' maps to the add_expense view function in the views module
     path('add-expense', views.add_expense, name="add-expenses")
 ]
