b7f69df154102a22df4c041341e306da26dffdc7
Ivan <58389741+YngBigBoy@users.noreply.github.com>
2024-05-19 09:42:21 +0300

    comment

--- bd_kr_service/authentication/urls.py
+++ bd_kr_service/authentication/urls.py
@@ -1,7 +1,19 @@
+# Import necessary modules and views
 from .views import LoginView, RegisterView
 from django.urls import path
 
+# Define the URL patterns for the application
 urlpatterns = [
-    path('register', RegisterView.as_view(), name='register'),
-    path('login', LoginView.as_view(), name='login')
+    # Define a URL pattern for registering a new user
+    path('register', 
+         # Use the RegisterView from the views.py file
+         RegisterView.as_view(), 
+         # Set the name of the view for reverse URL lookups
+         name='register'),
+    # Define a URL pattern for logging in an existing user
+    path('login', 
+         # Use the LoginView from the views.py file
+         LoginView.as_view(), 
+         # Set the name of the view for reverse URL lookups
+         name='login')
 ]
